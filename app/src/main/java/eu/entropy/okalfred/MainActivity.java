7a42d616792f001784462da55459541ca681f9db
Laury Lafage <lowrey97x@hotmail.fr>
2017-05-29 12:59:55 +0200

    Request from intent.

--- app/src/main/java/eu/entropy/okalfred/MainActivity.java
+++ app/src/main/java/eu/entropy/okalfred/MainActivity.java
@@ -5,19 +5,12 @@ import android.app.assist.AssistContent;
 import android.content.Intent;
 import android.os.Bundle;
 
-import java.io.IOException;
-import java.util.prefs.Preferences;
-
 import eu.entropy.okalfred.eu.entropy.okalfred.apiai.model.ApiAiResponse;
-import okhttp3.Request;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
-import static android.R.string.ok;
 
 public class MainActivity extends Activity {
 
@@ -25,18 +18,26 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (!getIntent().hasExtra(Intent.EXTRA_TEXT)) {
+            return;
+        }
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.api.ai/v1/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         ApiAiService service = retrofit.create(ApiAiService.class);
-        Call<ApiAiResponse> call = service.query("Bearer 30bf818339364a249da4d64c5160ab8f", "Shutdown the TV");
+
+        Intent t = getIntent();
+        String s = t.getStringExtra(Intent.EXTRA_TEXT);
+        System.out.println("===" + s);
+        Call<ApiAiResponse> call = service.query("Bearer 30bf818339364a249da4d64c5160ab8f", s);
         call.enqueue(new Callback<ApiAiResponse>() {
             @Override
             public void onResponse(Call<ApiAiResponse> call, Response<ApiAiResponse> response) {
                 ApiAiResponse myItem=response.body();
-                System.out.println(myItem.getSpeech());
+                System.out.println(myItem.getAction());
             }
 
             @Override
@@ -44,10 +45,6 @@ public class MainActivity extends Activity {
                 System.out.println(t);
             }
         });
-
-        Intent t = getIntent();
-        String s = t.getStringExtra(Intent.EXTRA_TEXT);
-        System.out.println("===" + s);
     }
 
     @Override
